9e02f0871915f59825959af0f48575c71bfdbb87
nandish.desai <ndesai.ittech@gmail.com>
2016-11-10 15:03:01 -0500

    Lecture 84 Completed

--- src/app/home-component.component.ts
+++ src/app/home-component.component.ts
@@ -10,20 +10,29 @@ import { Subscription } from 'rxjs/Rx';
     </h1>
     <hr>
     {{param}}
+    <hr>
+    {{fragement}}
   `,
   styles: []
 })
 export class HomeComponent implements OnDestroy {
 
   private subscription : Subscription;
+  private fragementSubscription: Subscription;
   param: string;
+  fragement: string;
   constructor(private route: ActivatedRoute){
     this.subscription = this.route.queryParams.subscribe(
         queryParam => this.param = queryParam['analytics']
+    );
+
+    this.fragementSubscription = this.route.fragment.subscribe(
+        fragment => this.fragement = fragment 
     );
   }
 
   ngOnDestroy(){
     this.subscription.unsubscribe();
+    this.fragementSubscription.unsubscribe();
   }
 }
